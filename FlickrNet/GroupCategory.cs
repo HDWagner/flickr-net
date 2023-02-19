b7a3c073d3b6bb7de16e9178d8c302cabb9a6afc
Wagner Harald <harald.d.wagner@googlemail.com>
2023-02-19 11:28:09 +0100

    fixed parsing Group. moved some classes into their own files. https://github.com/users/HDWagner/projects/1/views/1?pane=issue&itemId=20273751

--- FlickrNet/GroupCategory.cs
+++ FlickrNet/GroupCategory.cs
@@ -92,64 +92,13 @@
                 else
                 {
                     var s = new Group();
-                    ((IFlickrParsable)s).Load(reader); // <---- SonarQube says: here we might have a problem!!!!
+                    ((IFlickrParsable)s).Load(reader);
                     Groups.Add(s);
                 }
             }
 
             // Skip to next element (if any)
             reader.Skip();
-        }
-    }
-
-    /// <summary>
-    /// Holds details of a sub category, including its id, name and the number of groups in it.
-    /// </summary>
-    public sealed class Subcategory : IFlickrParsable
-    {
-        /// <summary>
-        /// The id of the category.
-        /// </summary>
-        public string SubcategoryId { get; set; }
-    
-        /// <summary>
-        /// The name of the category.
-        /// </summary>
-        public string SubcategoryName { get; set; }
-    
-        /// <summary>
-        /// The number of groups found within the category.
-        /// </summary>
-        public int GroupCount { get; set; }
-
-        void IFlickrParsable.Load(System.Xml.XmlReader reader)
-        {
-            if (reader.LocalName != "category")
-            {
-                UtilityMethods.CheckParsingException(reader);
-            }
-
-            while (reader.MoveToNextAttribute())
-            {
-                switch (reader.LocalName)
-                {
-                    case "id":
-                        SubcategoryId = reader.Value;
-                        break;
-                    case "name":
-                        SubcategoryName = reader.Value;
-                        break;
-                    case "count":
-                        GroupCount = int.Parse(reader.Value, System.Globalization.NumberFormatInfo.InvariantInfo);
-                        break;
-                    default:
-                        UtilityMethods.CheckParsingException(reader);
-                        break;
-
-                }
-            }
-
-            reader.Read();
         }
     }
 
